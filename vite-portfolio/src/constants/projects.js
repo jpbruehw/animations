9109c1fc89ff3c341680b4b4a2aa91a46ec4e722
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-06-20 22:02:57 +0200

    added tags to project logo

---
+++ vite-portfolio/src/constants/projects.js
@@ -0,0 +1,89 @@
+export const myProjects = [
+    {
+        title: "JobWorx - Job Search and Resume Builder App",
+        desc: "JobWorx is a one-of-a-kind job search platform that makes hiring and finding a new job easy. Take advantage of our profile builder to create your very own digital resume and make yourself accessible to top companies. For hiring managers, find candidates at your own pace without the spam and fluff of LinkedIn or Indeed.",
+        subDesc: "Built with React, Node, Express, JavaScript, Bootstrap, and PostgreSQL",
+        href: "https://github.com/jpbruehw/jobworx-production",
+        githubHref: "",
+        logo: "/assets/projects/logos/jobworx.svg",
+        logoStyle: {
+            // backgroundColor: '#2A1816',
+            border: '0.2px solid #36201D',
+            boxShadow: '0px 0px 60px 0px #AA3C304D',
+        },
+        spotlight: "/assets/projects/spotlights/spotlight1.png",
+        tags: [
+            {
+                id: 1,
+                name: "React.js",
+                path: "/assets/projects/tags/react.svg"
+            },
+            {
+                id: 2,
+                name: "NodeJS",
+                path: "/assets/projects/tags/node.svg"
+            },
+            {
+                id: 3,
+                name: "Express",
+                path: "/assets/projects/tags/expressjs.svg"
+            },
+            {
+                id: 4,
+                name: "JavaScript",
+                path: "/assets/projects/tags/js.svg"
+            },
+            {
+                id: 5,
+                name: "PostgreSQL",
+                path: "/assets/projects/tags/postgresql.svg"
+            }
+        ]
+    },
+    {
+        title: "Caesar Cipher",
+        desc: "Need a new password idea? Want to pass a secret message? Use this handy Caesar Cipher app to seamlessly encrypt and decrypt messages to both string and integer outputs. First used by Julius Caesar to securely pass military messages during times of conflict.",
+        subDesc: "Built with HTML, CSS, JavaScript, and Vite.",
+        href: "https://jpbruehw.github.io/caesar-cipher/",
+        githubHref: "https://github.com/jpbruehw/caesar-cipher/tree/main",
+        logoStyle: {
+            // backgroundColor: '#2A1816',
+            border: '0.2px solid #36201D',
+            boxShadow: '0px 0px 60px 0px #AA3C304D',
+        },
+        spotlight: "/assets/projects/spotlights/spotlight2.png",
+        tags: [
+            {
+                id: 1,
+                name: "HTML/CSS",
+                path: "/assets/projects/tags/html-css.svg"
+            },
+            {
+                id: 2,
+                name: "JavaScript",
+                path: "/assets/projects/tags/js.svg"
+            },
+            {
+                id: 3,
+                name: "Vite.js",
+                path: "/assets/projects/tags/vite.svg"
+            }
+        ]
+    },
+    {
+        title: "Modelling Pension Fund Solvency in Python",
+        desc: "Pension funds represent the backbone of the retirement system, allowing retirees to live out their golden years in dignity. As modelling techniques evolve, it is crucial to think bigger and differently about how we conduct analysis for pension funds. In this case, I used Monte Carlo simulations and scenario analysis to model the solvency of a pension fund and find optimal asset allocations using a solver-based approach.",
+        subDesc: "Built case study with Python, Pandas, Numpy, Matplotlib, Conda, and Spyder IDE",
+        href: "https://drive.google.com/file/d/1Llka_s-Fui-vqa5ijGYKfWeh7V7eYAn-/view",
+        githubHref: "https://github.com/jpbruehw/pension-fund-solvency-sim/tree/main",
+        logoStyle: {
+            // backgroundColor: '#2A1816',
+            border: '0.2px solid #36201D',
+            boxShadow: '0px 0px 60px 0px #AA3C304D',
+        },
+        spotlight: "/assets/projects/spotlights/spotlight3.png",
+        tags: [
+            
+        ]
+    }
+]
