aa69108340febe5db9efe25a5a57f657c4ee8ae0
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-06-22 14:40:02 +0200

    made navbar edits and also other changes to the project section

--- vite-portfolio/src/constants/projects.js
+++ vite-portfolio/src/constants/projects.js
@@ -1,259 +1,263 @@
 export const myProjects = [
     {
+        isData: false,
         title: "JobWorx - Job Search and Resume Builder App",
         desc: "JobWorx is a one-of-a-kind job search platform that makes hiring and finding a new job easy. Take advantage of our profile builder to create your very own digital resume and make yourself accessible to top companies. For hiring managers, find candidates at your own pace without the spam and fluff of LinkedIn or Indeed.",
         subDesc: "Built with React, Node, Express, JavaScript, Bootstrap, and PostgreSQL",
         href: "https://github.com/jpbruehw/jobworx-production",
         githubHref: "https://github.com/jpbruehw/jobworx-production",
-        logo: "/assets/projects/logos/jobworx.svg",
+        logo: "/projects/logos/jobworx.svg",
         logoStyle: {
             // backgroundColor: '#2A1816',
             border: '0.2px solid #36201D',
             boxShadow: '0px 0px 60px 0px #AA3C304D',
         },
-        spotlight: "/assets/projects/spotlights/spotlight1.png",
+        spotlight: "/projects/spotlights/spotlight1.png",
         tags: [
             {
                 id: 1,
                 name: "React.js",
-                path: "/assets/projects/tags/react.svg"
+                path: "/projects/tags/react.svg"
             },
             {
                 id: 2,
                 name: "NodeJS",
-                path: "/assets/projects/tags/node.svg"
+                path: "/projects/tags/node.svg"
             },
             {
                 id: 3,
                 name: "Express",
-                path: "/assets/projects/tags/expressjs.svg"
+                path: "/projects/tags/expressjs.svg"
             },
             {
                 id: 4,
                 name: "JavaScript",
-                path: "/assets/projects/tags/js.svg"
+                path: "/projects/tags/js.svg"
             },
             {
                 id: 5,
                 name: "PostgreSQL",
-                path: "/assets/projects/tags/postgresql.svg"
+                path: "/projects/tags/postgresql.svg"
             },
             {
                 id: 6,
                 name: "VSCode",
-                path: "/assets/projects/tags/vscode.svg"
+                path: "/projects/tags/vscode.svg"
             }
         ]
     },
-    {
+    {   
+        isData: false,
         title: "Caesar Cipher",
         desc: "Need a new password idea? Want to pass a secret message? Use this handy Caesar Cipher app to seamlessly encrypt and decrypt messages to both string and integer outputs. First used by Julius Caesar to securely pass military messages during times of conflict.",
         subDesc: "Built with HTML, CSS, JavaScript, and Vite.",
         href: "https://jpbruehw.github.io/caesar-cipher/",
         githubHref: "https://github.com/jpbruehw/caesar-cipher/tree/main",
-        logo: "/assets/projects/logos/caesar-cipher.svg",
+        logo: "/projects/logos/caesar-cipher.svg",
         logoStyle: {
             // backgroundColor: '#2A1816',
             border: '0.2px solid #36201D',
             boxShadow: '0px 0px 60px 0px #AA3C304D',
         },
-        spotlight: "/assets/projects/spotlights/spotlight2.png",
+        spotlight: "/projects/spotlights/spotlight2.png",
         tags: [
             {
                 id: 1,
                 name: "HTML/CSS",
-                path: "/assets/projects/tags/html-css.svg"
+                path: "/projects/tags/html-css.svg"
             },
             {
                 id: 2,
                 name: "JavaScript",
-                path: "/assets/projects/tags/js.svg"
+                path: "/projects/tags/js.svg"
             },
             {
                 id: 3,
                 name: "Vite.js",
-                path: "/assets/projects/tags/vite.svg"
+                path: "/projects/tags/vite.svg"
             },
             {
                 id: 4,
                 name: "VSCode",
-                path: "/assets/projects/tags/vscode.svg"
+                path: "/projects/tags/vscode.svg"
             }
         ]
     },
     {
+        isData: true,
         title: "Modelling Pension Fund Solvency in Python",
         desc: "Pension funds represent the backbone of the retirement system, allowing retirees to live out their golden years in dignity. As modelling techniques evolve, it is crucial to think bigger and differently about how we conduct analysis for pension funds. In this case, I used Monte Carlo simulations and scenario analysis to model the solvency of a pension fund and find optimal asset allocations using a solver-based approach.",
         subDesc: "Built case study with Python, Pandas, Numpy, Matplotlib, Conda, and Spyder IDE",
         href: "https://drive.google.com/file/d/1Llka_s-Fui-vqa5ijGYKfWeh7V7eYAn-/view",
         githubHref: "https://github.com/jpbruehw/pension-fund-solvency-sim/tree/main",
-        logo: "/assets/projects/logos/pension-fund.svg",
+        logo: "/projects/logos/pension-fund.svg",
         logoStyle: {
             // backgroundColor: '#2A1816',
             border: '0.2px solid #36201D',
             boxShadow: '0px 0px 60px 0px #AA3C304D',
         },
-        spotlight: "/assets/projects/spotlights/spotlight3.png",
+        spotlight: "/projects/spotlights/spotlight3.png",
         tags: [
             {
                 id: 1,
                 name: "Python",
-                path: "/assets/projects/tags/python.svg"
+                path: "/projects/tags/python.svg"
             },
             {
                 id: 2,
                 name: "Pandas",
-                path: "/assets/projects/tags/pandas.svg"
+                path: "/projects/tags/pandas.svg"
             },
             {
                 id: 3,
                 name: "Numpy",
-                path: "/assets/projects/tags/numpy.svg"
+                path: "/projects/tags/numpy.svg"
             },
             {
                 id: 4,
                 name: "Matplotlib",
-                path: "/assets/projects/tags/matplotlib.svg"
+                path: "/projects/tags/matplotlib.svg"
             },
             {
                 id: 5,
                 name: "Anaconda",
-                path: "/assets/projects/tags/conda.svg"
+                path: "/projects/tags/conda.svg"
             },
             {
                 id: 6,
                 name: "Spyder IDE",
-                path: "/assets/projects/tags/spyder.svg"
+                path: "/projects/tags/spyder.svg"
             }
         ]
     },
     {
+        isData: true,
         title: "Strategic Asset Allocation with Solver Based Optimization",
-        desc: (
-                <>
-                    Implemented <a href="https://www.cvxpy.org/tutorial/intro/index.html" target="_blank" rel="noopener noreferrer" className="underline text-blue-500">CVXPY</a> convex optimization package to simulate and build risk/return optimal portfolios using the Efficient Frontier and Capital Allocation Line. This methodology allows for more accurate forecasting beyond traditional approaches.
-                </>
-            ),
+        desc: "",
         subDesc: "Built case study with Python, Pandas, Numpy, CVXPY, Conda, and Spyder IDE",
         href: "https://drive.google.com/file/d/14LRSIdU0XA_VRfi9E6oOWNGBoK6ym6iK/view",
         githubHref: "https://github.com/jpbruehw/hbs-investor-case",
-        logo: "/assets/projects/logos/asset-allocation.svg",
+        logo: "/projects/logos/asset-allocation.svg",
         logoStyle: {
             // backgroundColor: '#2A1816',
             border: '0.2px solid #36201D',
             boxShadow: '0px 0px 60px 0px #AA3C304D',
         },
-        spotlight: "/assets/projects/spotlights/spotlight4.png",
+        spotlight: "/projects/spotlights/spotlight4.png",
         tags: [
             {
                 id: 1,
                 name: "Python",
-                path: "/assets/projects/tags/python.svg"
+                path: "/projects/tags/python.svg"
             },
             {
                 id: 2,
                 name: "Pandas",
-                path: "/assets/projects/tags/pandas.svg"
+                path: "/projects/tags/pandas.svg"
             },
             {
                 id: 3,
                 name: "Numpy",
-                path: "/assets/projects/tags/numpy.svg"
+                path: "/projects/tags/numpy.svg"
             },
             {
                 id: 4,
                 name: "CVXPY",
-                path: "/assets/projects/tags/cvxpy.svg"
+                path: "/projects/tags/cvxpy.svg"
             },
             {
                 id: 5,
                 name: "Anaconda",
-                path: "/assets/projects/tags/conda.svg"
+                path: "/projects/tags/conda.svg"
             },
             {
                 id: 6,
                 name: "Spyder IDE",
-                path: "/assets/projects/tags/spyder.svg"
+                path: "/projects/tags/spyder.svg"
             }
         ]
     },
     {
+        isData: false,
         title: "Typeframe",
         desc: "Ever feel like your frontend framework is bloated and too confusing to configure? Me too. That's where Typeframe comes in. TypeFrame is a custom built and lightweight TypeScript framework. Fully customizable, modular, and with seamless integration with 3rd party libraries, TypeFrame is perfect framework package for large and small projects alike.",
         subDesc: "Built with Typescript, VSCode, and HTML/CSS",
         href: "https://jpbruehw.github.io/typeframe/",
         githubHref: "https://github.com/jpbruehw/typeframe",
-        logo: "/assets/projects/logos/typeframe.svg",
+        logo: "/projects/logos/typeframe.svg",
         logoStyle: {
             // backgroundColor: '#2A1816',
             border: '0.2px solid #36201D',
             boxShadow: '0px 0px 60px 0px #AA3C304D',
         },
-        spotlight: "/assets/projects/spotlights/spotlight5.png",
+        spotlight: "/projects/spotlights/spotlight5.png",
         tags: [
             {
                 id: 1,
                 name: "Typescript",
-                path: "/assets/projects/tags/typescript.svg"
+                path: "/projects/tags/typescript.svg"
             },
             {
                 id: 2,
                 name: "VSCode",
-                path: "/assets/projects/tags/vscode.svg"
+                path: "/projects/tags/vscode.svg"
             },
             {
                 id: 3,
                 name: "HTML/CSS",
-                path: "/assets/projects/tags/html-css.svg"
+                path: "/projects/tags/html-css.svg"
             }
         ]
     },
     {
+        isData: true,
         title: "Predicting Mutual Fund Performance with Machine Learning Algorithms",
         desc: "Mutual funds chase alpha. Investors chase mutual funds that chase alpha. What factors drive outperformance? Can you predict it? That is exactly the question we asked here in this paper I co-authored, investigating the power of machine learning models to predict the performance of actively managed mutual funds.",
         subDesc: "Built case study with Python, Pandas, Numpy, Scipy, Pytorch, Anaconda, and Spyder IDE",
-        logo: "/assets/projects/logos/mutual-funds.svg",
+        href: "https://drive.google.com/file/d/11kH8Cxm42ZPUiMgA4Ofpbj3_69xH89_D/view",
+        githubHref: "https://github.com/jpbruehw/machine-learning-horse-race",
+        logo: "/projects/logos/mutual-funds.svg",
         logoStyle: {
             // backgroundColor: '#2A1816',
             border: '0.2px solid #36201D',
             boxShadow: '0px 0px 60px 0px #AA3C304D',
         },
-        spotlight: "/assets/projects/spotlights/spotlight1.png",
+        spotlight: "/projects/spotlights/spotlight1.png",
         tags: [
             {
                 id: 1,
                 name: "Python",
-                path: "/assets/projects/tags/python.svg"
+                path: "/projects/tags/python.svg"
             },
             {
                 id: 2,
                 name: "Pandas",
-                path: "/assets/projects/tags/pandas.svg"
+                path: "/projects/tags/pandas.svg"
             },
             {
                 id: 3,
                 name: "Numpy",
-                path: "/assets/projects/tags/numpy.svg"
+                path: "/projects/tags/numpy.svg"
             },
             {
                 id: 4,
                 name: "PyTorch",
-                path: "/assets/projects/tags/pytorch.svg"
+                path: "/projects/tags/pytorch.svg"
             },
             {
                 id: 5,
                 name: "SciPy",
-                path: "/assets/projects/tags/scipy.svg"
+                path: "/projects/tags/scipy.svg"
             },
             {
                 id: 6,
                 name: "Anaconda",
-                path: "/assets/projects/tags/conda.svg"
+                path: "/projects/tags/conda.svg"
             },
             {
                 id: 7,
                 name: "Spyder IDE",
-                path: "/assets/projects/tags/spyder.svg"
+                path: "/projects/tags/spyder.svg"
             }
         ]
     }
--- vite-portfolio/src/sections/projects/Projects.jsx
+++ vite-portfolio/src/sections/projects/Projects.jsx
@@ -3,19 +3,105 @@
  *  List projects in the same way as structured here - you can probably recycle from previous 
  *  add the blackjack game below this
  */
+import { Suspense, useState } from "react";
+import { myProjects } from "../../constants/projects"
+import { Canvas } from "@react-three/fiber";
+import { Center, OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../../components/CanvasLoader.jsx"
+import DemoComputer from "../../components/DemoComputer.jsx";
+
+const projectCount = myProjects.length
+
 function Projects() {
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(0)
+    const currentProject = myProjects[selectedProjectIndex]
+    
+    const handleNavigation = (direction) => {
+        setSelectedProjectIndex((prevIndex) => {
+            if (direction === 'previous'){
+                return prevIndex === 0 ? projectCount - 1 : prevIndex - 1
+            } else {
+                return prevIndex === projectCount - 1 ? 0 : prevIndex + 1
+            }
+        })
+    }
+
+    const cvxpyDesc = (
+                        <>
+                            Implemented{' '}
+                            <a
+                                href="https://www.cvxpy.org/tutorial/intro/index.html"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="underline text-blue-500"
+                            >
+                            CVXPY
+                            </a>{' '}
+                            convex optimization package to simulate and build risk/return optimal portfolios using the Efficient Frontier and Capital Allocation Line. This methodology allows for more accurate forecasting beyond traditional approaches.
+                        </>
+                    );
+
+    
     return (
         <section className="c-space my-20">
             <div className="">
                 <p className="head-text">
                     Some of my favorite projects
                 </p>
-                <p className="projects-subtext">klsajdhflak</p>
+                <p className="projects-subtext">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sit quisquam tempora iusto, ipsam quos beatae pariatur eius deleniti atque fuga, officiis voluptate voluptas quas corrupti. Deleniti inventore magnam ex!</p>
             </div>
             <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full">
                 <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
                     <div className="absolute top-0 right-0">
-
+                        <img src={currentProject.spotlight} alt="spotlight" className="w-full h-96 object-cover rounded-xl"/>
+                    </div>
+                    <div className="backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={currentProject.logoStyle}>
+                        <img src={currentProject.logo} alt="project-logo" className="w-16 h-16 rounded-lg shadow-sm"/>
+                    </div>
+                    <div className="flex flex-col gap-5 text-white-600 my-5">
+                        <p className="text-white text-2xl font-semibold animatedText">{currentProject.title}</p>
+                        <p className="animatedText">{currentProject.desc === "" ? cvxpyDesc : currentProject.desc}</p>
+                        <p className="animatedText">{currentProject.subDesc}</p>
+                    </div>
+                    <div className="flex items-center justify-between flex-wrap gap-5">
+                        <div className="flex items-center gap-4">
+                            {currentProject.tags.map((tag) => (
+                                <div key={tag.id} className="tech-logo">
+                                    <img src={tag.path} alt={tag.name} className="rounded-lg"/>
+                                </div>
+                            ))}
+                            <div className="flex flex-col items-end justify-center gap-2 ml-4">
+                                <a href={currentProject.href} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 cursor-pointer text-white-600">
+                                    <p>{currentProject.isData ? "View Report" : "View Demo"}</p>
+                                    <img src="assets/arrow-up.png" alt="arrow-up" className="w-3 h-3"/>
+                                </a>
+                                <a href={currentProject.githubHref} target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 cursor-pointer text-white-600">
+                                    <p>View on GitHub</p>
+                                    <img src="assets/arrow-up.png" alt="arrow-up" className="w-3 h-3"/>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center mt-7">
+                        <button className="arrow-btn" onClick={() => handleNavigation('previous')}>
+                            <img src="/assets/left-arrow.png" alt="left arrow" className="w-4 h-4" />
+                        </button>
+                        <button className="arrow-btn" onClick={() => handleNavigation('next')}>
+                            <img src="/assets/right-arrow.png" alt="left arrow" className="w-4 h-4" />
+                        </button>
+                    </div>
+                    <div className="border border-black-300 rounded-lg h-96 md:h-full">
+                        <Canvas>
+                            <ambientLight intensity={3} />
+                            <directionalLight position={[10, 10, 5]} />
+                            <Center>
+                                <Suspense fallbackBack={<CanvasLoader />} />
+                                <group scale={2} position={[0, -3, 0]} rotation={[0, -0.1, 0]}>
+                                    <DemoComputer />
+                                </group>
+                            </Center>
+                            <OrbitControls maxPolarAngle={Math.PI / 2} enableZoom={false} />
+                        </Canvas>
                     </div>
                 </div>
             </div>
