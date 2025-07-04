237ead6469ab7b11685d7b20fbfc5740212fcc40
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-07-04 15:17:29 +0200

    updated blackjack game

--- vite-portfolio/src/sections/blackjack-minigame/BlackjackWrapper.jsx
+++ vite-portfolio/src/sections/blackjack-minigame/BlackjackWrapper.jsx
@@ -2,9 +2,20 @@ import BlackjackGame from "./Blackjack"
 
 function BlackjackWrapper() {
     return (
-        <div className="w-full h-screen">
+        <section className="flex flex-col items-center justify-center w-full h-screen">
+            <div className="flex flex-col items-start justify-start w-full c-space gap-4">
+                <p className="head-text">
+                    Wait! Before you keep scrolling...want to play a game?
+                </p>
+                <p className="projects-subtext">
+                    Is success luck or skill? I would argue that it is a mix of both. <b>However, the harder you work, the luckier you get.</b> 
+                </p>
+                <p>
+                    
+                </p>
+            </div>
             <BlackjackGame />
-        </div>
+        </section>
     )
 }
 
