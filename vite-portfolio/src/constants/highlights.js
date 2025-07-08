65ed10d55d4d16cd0b499f88188ba3174527f6dd
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-07-08 22:44:14 +0200

    edited highlights content

--- vite-portfolio/src/constants/highlights.js
+++ vite-portfolio/src/constants/highlights.js
@@ -1,34 +1,58 @@
 export const highlights = [
     {
         id: 1,
-        name: 'Emily Johnson',
-        position: 'Marketing Director at GreenLeaf',
-        img: 'assets/review1.png',
-        review:
-        'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+        header: 'Derivative Trading Game Winner',
+        subheader: 'Saxo Bank Sponsored Program',
+        img: '/highlights/saxo-bank.svg',
+        desc:
+        '',
+        links: [
+            {
+                id: 1,
+                text: 'LinkedIn Post',
+                href: ''
+            },
+            {
+                id: 2,
+                text: 'Read Report',
+                href: ''
+            }
+        ]
     },
     {
         id: 2,
-        name: 'Mark Rogers',
-        position: 'Founder of TechGear Shop',
-        img: 'assets/review2.png',
-        review:
-        'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+        header: 'Python Case Studies',
+        subheader: 'Collaborative Project with HSLU Faculty',
+        img: '/highlights/case-study.svg',
+        desc:
+        '',
+        links: [
+            {
+                id: 1,
+                text: 'GitHub Repo',
+                href: ''
+            },
+            {
+                id: 2,
+                text: 'Read Report',
+                href: ''
+            }
+        ]
     },
     {
         id: 3,
-        name: 'John Dohsas',
-        position: 'Project Manager at UrbanTech ',
+        header: 'John Dohsas',
+        subheader: 'Project Manager at UrbanTech ',
         img: 'assets/review3.png',
-        review:
+        desc:
         'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
     },
     {
         id: 4,
-        name: 'Ether Smith',
-        position: 'CEO of BrightStar Enterprises',
+        header: 'Ether Smith',
+        subheader: 'CEO of BrightStar Enterprises',
         img: 'assets/review4.png',
-        review:
+        desc:
         'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
     },
 ];
