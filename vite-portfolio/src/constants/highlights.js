9cbfa96ca1e4491f05d4c86ca1c3bafe23215ef3
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-07-11 10:55:20 +0200

    added more highlights information to page

--- vite-portfolio/src/constants/highlights.js
+++ vite-portfolio/src/constants/highlights.js
@@ -1,21 +1,16 @@
 export const highlights = [
     {
         id: 1,
-        header: 'Derivative Trading Game Winner',
-        subheader: 'Saxo Bank Sponsored Program',
+        header: 'Saxo Bank Trading Game Winner',
+        subheader: 'Saxo Bank Sponsored Partnership with HSLU',
         img: '/highlights/saxo-bank.svg',
         desc:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Lead a team to win the very first Saxo Bank Trading Game award. Several teams were given a "one million CHF" to trade on the Saxo Bank platform and subsequently write a report. I lead my team to win by crafting a structured approach to decision making and conflict resolution within the team. With my system, we were able to defeat the competition through superior organization and decision quality, culminating in a 120+ page report that handily propelled us to victory.',
         links: [
             {
                 id: 1,
                 text: 'LinkedIn Post',
-                href: ''
-            },
-            {
-                id: 2,
-                text: 'Read Report',
-                href: ''
+                href: 'https://www.linkedin.com/posts/mscbf_nextgeneration-saxobeinvested-activity-7039594445792509952-Cyzi/?utm_source=share&utm_medium=member_desktop'
             }
         ]
     },
@@ -30,12 +25,7 @@ export const highlights = [
             {
                 id: 1,
                 text: 'GitHub Repo',
-                href: ''
-            },
-            {
-                id: 2,
-                text: 'Read Report',
-                href: ''
+                href: 'https://github.com/jpbruehw/python-case-studies'
             }
         ]
     },
