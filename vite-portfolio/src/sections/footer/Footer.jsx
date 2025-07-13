18d1e117b01387b72fc470da58bc781d41dfa611
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-07-13 21:23:26 +0200

    finished footer section

--- vite-portfolio/src/sections/footer/Footer.jsx
+++ vite-portfolio/src/sections/footer/Footer.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 function Footer() {
+
+    const [hasCopied, setHasCopied] = useState(false);
+
+    const handleCopy = () => {
+    navigator.clipboard.writeText('jpbruehwiler@icloud.com');
+        setHasCopied(true);
+        setTimeout(() => {
+            setHasCopied(false);
+        }, 2000);
+    };
+
     return (
-        <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
-            <p lassName="text-white-500">
-                Add persnal motto here
-            </p>
-            <div className="flex items-center gap-3">
-                
+        <footer className="c-space pt-7 pb-3 border-t border-white-500 flex justify-between items-center flex-wrap gap-5">
+            <div className="flex items-center gap-5">
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                    Let's Connect:
+                </p>
                 <a
                     href="https://github.com/jpbruehw"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="social-icon"
                 >
-                    <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
+                    <img src="/assets/github.png" alt="github" className="h-full rounded-full transition-transform duration-200 ease-in-out hover:scale-110" />
                 </a>
 
                 <a
@@ -21,21 +33,20 @@ function Footer() {
                     rel="noopener noreferrer"
                     className="social-icon"
                 >
-                    <img src="/assets/linkedin.svg" alt="linkedin" className="w-1/2 h-1/2" />
+                    <img src="/assets/linkedin.svg" alt="linkedin" className="h-full rounded-full transition-transform duration-200 ease-in-out hover:scale-110" />
                 </a>
 
-                <a
-                    href="https://instagram.com/yourusername"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="social-icon"
-                >
-                    <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-                </a>
+                
             </div>
+                <div className="copy-container" onClick={handleCopy}>
+                    <img src={hasCopied ? '/assets/tick.svg' : '/assets/copy.svg'} alt="copy" />
+                    <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                        jpbruehwiler@icloud.com
+                    </p>
+                </div>
 
-            <p className="text-white-500">
-                © 2025 Jean-Philipp Bruehwiler. All rights reserved.
+            <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">
+                © 2025 Jean-Philipp Bruehwiler.
             </p>
         </footer>
     );
