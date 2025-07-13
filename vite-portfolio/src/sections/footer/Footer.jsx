cea215fdf5073db32a745077438d082766e7526d
Jean-Philipp Bruehwiler <jpbruehwiler@gmail.com>
2025-07-13 19:21:11 +0200

    edited footer section

--- vite-portfolio/src/sections/footer/Footer.jsx
+++ vite-portfolio/src/sections/footer/Footer.jsx
@@ -1,22 +1,44 @@
-
-
 function Footer() {
     return (
         <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
-            <div className="flex gap-3">
-                <div className="social-icon">
+            <p lassName="text-white-500">
+                Add persnal motto here
+            </p>
+            <div className="flex items-center gap-3">
+                
+                <a
+                    href="https://github.com/jpbruehw"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="social-icon"
+                >
                     <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-                </div>
-                <div className="social-icon">
+                </a>
+
+                <a
+                    href="https://www.linkedin.com/in/jpbruehwiler/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="social-icon"
+                >
                     <img src="/assets/linkedin.svg" alt="linkedin" className="w-1/2 h-1/2" />
-                </div>
-                <div className="social-icon">
+                </a>
+
+                <a
+                    href="https://instagram.com/yourusername"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="social-icon"
+                >
                     <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-                </div>
-            </div>  
-            <p className="text-white-500">© 2025 Jean-Philipp Bruehwiler. All rights reserved.</p>
+                </a>
+            </div>
+
+            <p className="text-white-500">
+                © 2025 Jean-Philipp Bruehwiler. All rights reserved.
+            </p>
         </footer>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
